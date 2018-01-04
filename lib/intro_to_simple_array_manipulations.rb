3da9b5047d4415e1088e225a0dc600e53283fc27
York Wang <yw544@nyu.edu>
2018-01-04 19:12:45 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,33 +2,44 @@ def using_push(array,string)
 array.push(string)
 end
 
-def using_unshift(a,b)
+def using_unshift(array,string)
+array.unshift(string)
+end
+
+def using_pop(array)
+array.pop
+end
+
+def pop_with_args(array)
+array.pop(2)
+end
+
+def using_shift(array)
+array.shift
+end
+
+def using_shift_args(array)
+  array.shift(2)
+end
+
+def using_concat(array,array2)
+  array.concat(array2)
 
 end
-def using_pop
+
+def using_insert(array,element)
+  array.insert(4, element)
 
 end
-def pop_with_args
 
-end
-def using_unshift
-
-end
-def shift
-
-end
-def using_concat
-
-end
-def using_insert
-
-end
 def using_uniq
 
 end
+
 def using_flatten
 
 end
+
 def using_delete
 
 end
